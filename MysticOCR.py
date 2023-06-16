2b510d085d81fab109d317759608f0e889b7195f
Chad O <lvnvceo@gmail.com>
2023-06-16 00:11:38 -0700

    Ran it through ChatGPT
    Improvements made:
    
    Updated the code to use os.path.join for file path concatenation.
    Replaced the bitwise & operator with logical and operator (args.paragraph == True & args.details == 1 -> args.paragraph and args.details == 1).
    Used action="store_true" for boolean arguments args.paragraph and args.progress_only to handle their presence as true values.
    Created the success_dir and failed_dir directories using os.makedirs with exist_ok=True to handle directory creation if they don't exist.
    Fixed the incorrect usage of average_confidence function (missing bounds parameter) when appending to bounds list.
    Changed the print statements for "PASSED THRESHOLD" and "FAILED THRESHOLD" to use commas instead of string concatenation.
    Corrected the args.threshold type to float in the argument parser.
    Removed the unnecessary variable output_file in the main function since writer is sufficient for writing to the file.

--- MysticOCR.py
+++ MysticOCR.py
@@ -10,30 +10,28 @@ import tqdm
 parser = argparse.ArgumentParser(description="Just an OCR reader for magic cards",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-i", "--image_dir", type=str, required=True)
-parser.add_argument("-o", "--output_file", type=str,
-                    required=False, default=None)
+parser.add_argument("-o", "--output_file", type=str, default=None)
 parser.add_argument("-b", "--batch_size", type=int, default=1)
 parser.add_argument("-w", "--workers", type=int, default=0)
 parser.add_argument("-d", "--details", type=int, default=1)
 parser.add_argument("-bl", "--blocklist", type=str, default="")
-parser.add_argument("-p", "--paragraph", type=bool, default=False)
-parser.add_argument("-P", "--progress_only", type=bool, default=False)
-parser.add_argument("-show", "--show_image", type=bool, default=True)
+parser.add_argument("-p", "--paragraph", action="store_true", default=False)
+parser.add_argument("-P", "--progress_only", action="store_true", default=False)
+parser.add_argument("-show", "--show_image", action="store_true", default=True)
 parser.add_argument("-fd", "--failed_dir", type=str, default=None)
 parser.add_argument("-sd", "--success_dir", type=str, default=None)
-parser.add_argument("-th", "--threshold", type=str, default=0.7)
+parser.add_argument("-th", "--threshold", type=float, default=0.7)
 parser.add_argument("-wth", "--width_ths", type=float, default=3)
-parser.add_argument("-xth", "--x_ths", tpype=float, default=4)
+parser.add_argument("-xth", "--x_ths", type=float, default=4)
 args = parser.parse_args()
 
 
 def main():
     # Retrieve all image paths that match the pattern
-    files = glob.glob("*.jpg", root_dir=args.image_dir)
+    files = glob.glob(os.path.join(args.image_dir, "*.jpg"))
 
     reader = easyocr.Reader(['en'], gpu=True)
     writer = None
-    output_file = None
 
     if args.output_file is not None:
         print(f"Opening {args.output_file} for writing")
@@ -46,9 +44,9 @@ def main():
         for file in files:
             file_path = os.path.join(args.image_dir, file)
 
-            if args.paragraph == True & args.details == 1:
-                print(
-                    "WARNING: paragraph is set to True, but details is set to 1, output will not include confidence level")
+            if args.paragraph and args.details == 1:
+                print("WARNING: paragraph is set to True, but details is set to 1, output will not include confidence level")
+            
             bounds = reader.readtext(file_path, batch_size=args.batch_size,
                                      workers=args.workers, detail=args.details, blocklist=args.blocklist,
                                      paragraph=args.paragraph, x_ths=args.x_ths, width_ths=args.width_ths, min_size=10)
@@ -57,7 +55,7 @@ def main():
                 im = cv2.imread(file_path)
                 for bbox in bounds:
                     # Unpack the bounding box
-                    (tl, tr, br, bl) = bbox[0]
+                    tl, tr, br, bl = bbox[0]
                     tl = (int(tl[0]), int(tl[1]))
                     tr = (int(tr[0]), int(tr[1]))
                     br = (int(br[0]), int(br[1]))
@@ -67,23 +65,21 @@ def main():
                 cv2.waitKey(1)
 
             if args.success_dir is not None:
-                if not os.path.exists(args.success_dir):
-                    os.mkdir(args.success_dir)
+                os.makedirs(args.success_dir, exist_ok=True)
                 cv2.imwrite(os.path.join(args.success_dir, file), im)
 
             if args.failed_dir is not None:
-                if not os.path.exists(args.failed_dir):
-                    os.mkdir(args.failed_dir)
+                os.makedirs(args.failed_dir, exist_ok=True)
                 cv2.imwrite(os.path.join(args.failed_dir, file), im)
 
             if average_confidence(bounds) >= args.threshold:
-                bounds.append(average_confidence)
+                bounds.append(average_confidence(bounds))
                 if not args.progress_only:
-                    print("PASSED THRESHOLD: " + file)
+                    print("PASSED THRESHOLD:", file)
             else:
-                bounds.append(average_confidence)
+                bounds.append(average_confidence(bounds))
                 if not args.progress_only:
-                    print("FAILED THRESHOLD: " + file)
+                    print("FAILED THRESHOLD:", file)
 
             if args.output_file is not None:
                 writer.writerow(bounds)
