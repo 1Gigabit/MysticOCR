d756c6ad1e5256fe483346adbe5b0480d5a86a76
Chad O <lvnvceo@gmail.com>
2023-06-17 17:21:52 -0700

    Restructured code

--- MatchScryfall.py
+++ MatchScryfall.py
@@ -1,36 +1,53 @@
-from argparse import ArgumentParser
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 import ast
 import csv
 import json
 from fuzzywuzzy import fuzz
+# Define the command-line arguments
+parser = ArgumentParser(description="Similarity matcher for magic cards",
+                        formatter_class=ArgumentDefaultsHelpFormatter)
+parser.add_argument("-c", "--csv_file_path",
+                    help="Path to the CSV file", required=True)
+parser.add_argument("-db", "--database_file_path",
+                    help="Path to the database file", required=True)
 
-args = ArgumentParser()
-args.add_argument("-c", "--csv_file_path", help="Path to the CSV file", required=True)
-args.add_argument("-db","--database_file_path", help="Path to the database file", required=True)
-args.add_argument("-name","--name", help="Get names of cards from the CSV file", required=True)
-def compare_csv_with_json(csv_file_path, json_file_path, threshold_ratio):
+args = parser.parse_args()
+
+
+def main():
     # Load the JSON data from the JSON file
-    with open(json_file_path, encoding="utf-8") as json_file:
-        json_data = json.load(json_file)
-
+    try:
+        with open(args.database_file_path, 'r', encoding="utf-8") as json_file:
+            json_data = json.load(json_file)
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        print(f"Error loading JSON file: {e}")
+        return
     # Open the CSV file
-    with open(csv_file_path, 'r') as csv_file:
+    with open(args.csv_file_path, 'r') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=",")
+        next(csv_reader)  # Skip the header row
+
         # Iterate over each row in the CSV file
         for row in csv_reader:
-            if(len(row) != 0):
-                cellLiteral = ast.literal_eval(row[0])
-                name = cellLiteral[1]
-                print(name)
-                
+            if len(row) >= 1:
+                highest_ratio = 0
+                current_match = ""
+                for card in json_data:
+                    row_literal = ast.literal_eval(row[0])
+                    if isinstance(row_literal, int):
+                        break
+                    if(row_literal is int):
+                        break
+                    current_compare = card.get("name")
+                    current_ratio = fuzz. ratio(
+                        row_literal[1], current_compare)
+                    if current_ratio > highest_ratio:
+                        highest_ratio = current_ratio
+                        current_match = current_compare
+                    if current_ratio == 100:
+                        break
+                print(highest_ratio, current_match)
 
 
-# Provide th   e file paths for the CSV and JSON files
-csv_file_path = 'result.csv'
-json_file_path = 'db.json'
-
-# Set the threshold ratio for the match accuracy
-threshold_ratio = 20  # Adjust this value based on your requirements
-
-# Call the function to compare CSV with JSON
-compare_csv_with_json(csv_file_path, json_file_path, threshold_ratio)
+if __name__ == '__main__':
+    main()
