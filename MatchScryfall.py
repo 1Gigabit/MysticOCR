6c9969b3fc959686956bb1d6761c37463baccc72
Chad O <lvnvceo@gmail.com>
2023-06-17 23:55:35 -0700

    Reconstructed code
    Changes
    
    -- MytsticOCR.py --
    
    - Added logging method & calls
    - Added animated indicator
    - Restructured how it executes things
    - Changed --progress_only changed to --progress

--- MatchScryfall.py
+++ MatchScryfall.py
@@ -2,13 +2,14 @@ from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 import ast
 import csv
 import json
+import sqlite3
 from fuzzywuzzy import fuzz
 # Define the command-line arguments
 parser = ArgumentParser(description="Similarity matcher for magic cards",
                         formatter_class=ArgumentDefaultsHelpFormatter)
-parser.add_argument("-c", "--csv_file_path",
+parser.add_argument("-c", "--csv_file",
                     help="Path to the CSV file", required=True)
-parser.add_argument("-db", "--database_file_path",
+parser.add_argument("-db", "--database_file",
                     help="Path to the database file", required=True)
 
 args = parser.parse_args()
@@ -16,37 +17,43 @@ args = parser.parse_args()
 
 def main():
     # Load the JSON data from the JSON file
-    try:
-        with open(args.database_file_path, 'r', encoding="utf-8") as json_file:
-            json_data = json.load(json_file)
-    except (FileNotFoundError, json.JSONDecodeError) as e:
-        print(f"Error loading JSON file: {e}")
-        return
+    conn = sqlite3.connect(args.database_file)
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM data")
+
     # Open the CSV file
-    with open(args.csv_file_path, 'r') as csv_file:
+    with open(args.csv_file, 'r') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=",")
         next(csv_reader)  # Skip the header row
 
         # Iterate over each row in the CSV file
+        cards = cursor.fetchall()
         for row in csv_reader:
             if len(row) >= 1:
                 highest_ratio = 0
-                current_match = ""
-                for card in json_data:
-                    row_literal = ast.literal_eval(row[0])
-                    if isinstance(row_literal, int):
-                        break
-                    if(row_literal is int):
-                        break
-                    current_compare = card.get("name")
-                    current_ratio = fuzz. ratio(
-                        row_literal[1], current_compare)
+                image_text = ""
+                db_text = ""
+                row_literal = ast.literal_eval(row[0])
+                if isinstance(row_literal, int):
+                    break
+                if(row_literal is int):
+                    break
+                for card in cards:
+
+                    comparison = card[0] or ""
+                    current_ratio = fuzz.ratio(
+                        row_literal[1], comparison)
                     if current_ratio > highest_ratio:
                         highest_ratio = current_ratio
-                        current_match = current_compare
+                        image_text = row_literal
+                        db_text = comparison
+
+                    if comparison.find("//") != -1 and comparison.find(row_literal[1]) != -1:
+                        break
                     if current_ratio == 100:
                         break
-                print(highest_ratio, current_match)
+
+                print(f'{highest_ratio}%  :  {image_text}  :  {db_text}')
 
 
 if __name__ == '__main__':
--- MysticOCR.py
+++ MysticOCR.py
@@ -1,7 +1,12 @@
 import argparse
 import csv
 import glob
+import itertools
 import os
+import sys
+from threading import Thread
+import threading
+import time
 import easyocr
 import cv2
 import tqdm
@@ -16,80 +21,88 @@ parser.add_argument("-w", "--workers", type=int, default=0)
 parser.add_argument("-d", "--details", type=int, default=1)
 parser.add_argument("-bl", "--blocklist", type=str, default="")
 parser.add_argument("-p", "--paragraph", default=False)
-parser.add_argument("-P", "--progress_only", default=False)
+parser.add_argument("-P", "--progress", default=False)
 parser.add_argument("-show", "--show_image", default=True)
 parser.add_argument("-fd", "--failed_dir", type=str, default=None)
 parser.add_argument("-sd", "--success_dir", type=str, default=None)
 parser.add_argument("-th", "--threshold", type=float, default=0.6)
 parser.add_argument("-wth", "--width_ths", type=float, default=3)
 parser.add_argument("-xth", "--x_ths", type=float, default=4)
+parser.add_argument("-q", "--quiet", default=False)
 args = parser.parse_args()
 
 
-def main():
-    # Retrieve all image paths that match the pattern
-    files = glob.glob(os.path.join(args.image_dir, "*.jpg"))
+def log(message):
+    if args.quiet is False:
+        print(f'[{time.process_time()}] : {message}')
 
+
+def main():
+    log(f'Opening image_dir...')
+    files = glob.glob(os.path.join(args.image_dir, "*.jpg"))
+    log(f'Creating EasyOCR reader...')
     reader = easyocr.Reader(['en'], gpu=True)
     writer = None
-
+    pbar = None # Todo
+    log_file = None # Todo
+    done = False
+    file = None
+    count = 0
     if args.output_file is not None:
-        print(f"Opening {args.output_file} for writing")
+        log(f"Opening output CSV file for writing...")
         output_file = open(args.output_file, "w")
         writer = csv.writer(output_file)
+    if args.progress is True:
+        log(f'Creating progress bar...')
+        pbar = tqdm.tqdm(total=len(files)) # Todo
 
-    with tqdm.tqdm(total=len(files)) as pbar:
-        if not args.progress_only:
-            pbar.close()
-        for file in files:
+    def animated_loading():
+        for c in itertools.cycle(['|', '/', '-', '\\']):
+            if done:
+                break
+            sys.stdout.write(f'\rScanning images: {c} {count}/{len(files)} OR {round((count/len(files)*100),3)}% completed')
+            sys.stdout.flush()
+            time.sleep(0.1)
+        sys.stdout.write('\rDone!             ')
+    t = threading.Thread(target=animated_loading, daemon=True)
+    t.start()
+    for file in files:
+        count += 1
+        bounds = reader.readtext(file, batch_size=args.batch_size,
+                                 workers=args.workers, detail=args.details, blocklist=args.blocklist,
+                                 paragraph=args.paragraph, x_ths=args.x_ths, width_ths=args.width_ths, min_size=10)
+        if args.show_image:
+            im = cv2.imread(file)
+            for bbox in bounds:
+                # Unpack the bounding box
+                tl, tr, br, bl = bbox[0]
+                tl = (int(tl[0]), int(tl[1]))
+                tr = (int(tr[0]), int(tr[1]))
+                br = (int(br[0]), int(br[1]))
+                bl = (int(bl[0]), int(bl[1]))
+                cv2.rectangle(im, tl, br, (10, 255, 0), 2)
+            im = cv2.resize(im, (480, 600))
+            cv2.imshow('Image', im)
 
-            if args.paragraph and args.details == 1:
-                print("WARNING: paragraph is set to True, but details is set to 1, output will not include confidence level")
-            
-            bounds = reader.readtext(file, batch_size=args.batch_size,
-                                     workers=args.workers, detail=args.details, blocklist=args.blocklist,
-                                     paragraph=args.paragraph, x_ths=args.x_ths, width_ths=args.width_ths, min_size=10)
+            cv2.waitKey(1)  # 1 to make sure image updates
 
-            if args.show_image:
-                im = cv2.imread(file)
-                for bbox in bounds:
-                    # Unpack the bounding box
-                    tl, tr, br, bl = bbox[0]
-                    tl = (int(tl[0]), int(tl[1]))
-                    tr = (int(tr[0]), int(tr[1]))
-                    br = (int(br[0]), int(br[1]))
-                    bl = (int(bl[0]), int(bl[1]))
-                    cv2.rectangle(im, tl, br, (10, 255, 0), 2)
-                cv2.imshow('Image', im)
-                cv2.waitKey(1)
-            avg_confidence = calc_avg_confidence(bounds)
-            if avg_confidence >= args.threshold:
-                bounds.append(avg_confidence)
-                if not args.progress_only:
-                    print("PASSED THRESHOLD:", file)                        
-                if args.success_dir is not None:
-                    os.makedirs(args.success_dir, exist_ok=True)
-                    success_file = os.path.join(args.success_dir,f"{avg_confidence}__"+os.path.basename(file))
-                    cv2.imwrite(success_file,im)
-            else:
-                bounds.append(avg_confidence)
-                if not args.progress_only:
-                    print("FAILED THRESHOLD:", file)
-                if args.failed_dir is not None:
-                    os.makedirs(args.failed_dir, exist_ok=True)
-                    failed_file = os.path.join(args.failed_dir,f"{avg_confidence}__"+os.path.basename(file))
-                    print(failed_file)
-                    cv2.imwrite(failed_file,im)
-
-            if args.output_file is not None:
-                writer.writerow(bounds)
-
-            if args.progress_only:
-                pbar.update()
-            else:
-                print(bounds)
-                print("\n" * 2)
-                print('-' * 100)
+        avg_confidence = calc_avg_confidence(bounds)
+        bounds.append(avg_confidence)
+        if avg_confidence >= args.threshold:
+            if args.success_dir is not None:
+                os.makedirs(args.success_dir, exist_ok=True)
+                success_file = os.path.join(
+                    args.success_dir, f"{avg_confidence}__"+os.path.basename(file))
+                cv2.imwrite(success_file, im)
+        else:
+            if args.failed_dir is not None:
+                os.makedirs(args.failed_dir, exist_ok=True)
+                failed_file = os.path.join(
+                    args.failed_dir, f"{avg_confidence}__"+os.path.basename(file))
+                cv2.imwrite(failed_file, im)
+        if writer is not None:
+            writer.writerow(bounds)
+    done = True
 
 
 def calc_avg_confidence(bounds):
