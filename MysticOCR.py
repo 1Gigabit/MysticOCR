37c2de3b5a921afc46a422bfa4bbe82ba8886b02
Chad O <lvnvceo@gmail.com>
2023-06-23 12:29:24 -0700

    Clean up & added some columns.

--- MysticOCR.py
+++ MysticOCR.py
@@ -28,11 +28,10 @@ def scan():
     files = glob.glob(os.path.join(config['scan']['image_dir'], "*.jpg"))
     reader = easyocr.Reader(['en'], gpu=config['scan']['gpu'])
     db_connection = sqlite3.connect(config['scan']['output_db'])
-    db_cursor = db_connection.cursor()
     if config['overwrite_db'] is True:
         db_connection.execute('DROP TABLE IF EXISTS ocr_results;')
     db_connection.execute(
-        'CREATE TABLE IF NOT EXISTS ocr_results (id INTEGER PRIMARY KEY AUTOINCREMENT, file_name TEXT,card_type TEXT,foil TEXT,card_set TEXT,rarity TEXT,ocr_result TEXT);')
+        'CREATE TABLE IF NOT EXISTS ocr_results (id INTEGER PRIMARY KEY AUTOINCREMENT, file_name TEXT,location TEXT,foil TEXT, showcase TEXT,ocr_result TEXT);')
     done = False
     count = 0
 
@@ -50,9 +49,12 @@ def scan():
     t.start()
     for file in files:
         count += 1
-        result = reader.readtext(
-            file, width_ths=config['scan']['width_ths'], x_ths=config['scan']['x_ths'])
         imagecv = cv2.imread(file)
+        if (config['scan']['card']['sideways'] is True):
+            imagecv = cv2.rotate(imagecv, cv2.ROTATE_90_CLOCKWISE)
+
+        result = reader.readtext(
+            imagecv, width_ths=config['scan']['width_ths'], x_ths=config['scan']['x_ths'])
         if config['scan']['show_image'] is True:
             for bbox in result:
                 # Unpack the bounding box
@@ -61,19 +63,16 @@ def scan():
                 tr = (int(tr[0]), int(tr[1]))
                 br = (int(br[0]), int(br[1]))
                 bl = (int(bl[0]), int(bl[1]))
-                cv2.rectangle(imagecv.copy(),
-                              (tl[0], tl[1], br[1], br[0]), (10, 255, 0), 2)
+                cv2.rectangle(imagecv, tl, br, (10, 255, 0), 2)  # type: ignore
 
             imagecv = cv2.resize(imagecv, (480, 600))
             cv2.imshow('Image', imagecv)
             cv2.waitKey(1)  # 1 to make sure image updates
 
         db_connection.execute(
-            "INSERT INTO ocr_results (file_name,card_type,foil,card_set,rarity,ocr_result) VALUES (?,?,?,?,?,?);",
-            (file, config['scan']['card']['type'],
-             config['scan']['card']['foil'],
-             config['scan']['card']['set'],
-             config['scan']['card']['rarity'], f'{result}'))
+            "INSERT INTO ocr_results (file_name,location,foil,showcase,ocr_result) VALUES (?,?,?,?,?);",
+            (file, config['scan']['card']['location'],
+             config['scan']['card']['foil'], config['scan']['card']['showcase'], f'{result}'))
         db_connection.commit()
     done = True
 
@@ -88,24 +87,24 @@ def match():
     done = False
     count = 0
 
-    def animated_loading(length: int):
-        for c in itertools.cycle(['|', '/', '-', '\\']):
-            if done:
-                break
-            sys.stdout.write(
-                f'\rScanning images: {c} {count}/{length} OR {round((count/length*100),3)}% completed')
-            sys.stdout.flush()
-            time.sleep(0.1)
-        sys.stdout.write('\rDone!            ')
-    t = threading.Thread(target=animated_loading, args=[
-                         len(ocr_results)], daemon=True)
-    t.start()
-
     for result in ocr_results:
         count += 1
         highest_ratio = 0
-        ocr_1st_element = ast.literal_eval(result[0])
-        print(ocr_1st_element[0][1])
+        highest_card = dict()
+        ocr_1st_element = ast.literal_eval(result[0])[0][1]
+        ocr_2nd_element = ast.literal_eval(result[0])[1][1]
+
+        for card in card_db:
+            fuzz_ratio_1 = fuzz.ratio(ocr_1st_element, card.get('name'))
+            fuzz_ratio_2 = fuzz.ratio(ocr_2nd_element, card.get('name'))
+            fuzz_ratio_3 = fuzz.ratio(
+                ocr_1st_element + ocr_2nd_element, card.get('name'))
+            fuzz_ratio = max([fuzz_ratio_1, fuzz_ratio_2, fuzz_ratio_3])
+            if highest_ratio < fuzz_ratio:
+                highest_ratio = fuzz_ratio
+                highest_card = card
+        print(
+            f'\r\nhighest_ratio: {highest_ratio} :{highest_card.get("name")}')
     done = True
 
 
