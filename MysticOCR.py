9bdfbd704e37ce5d95fa91e55dd86bf15b4a3f0f
Chad O <lvnvceo@gmail.com>
2023-06-23 08:15:25 -0700

    Added matching

--- MysticOCR.py
+++ MysticOCR.py
@@ -1,4 +1,3 @@
-import argparse
 import glob
 import itertools
 import json
@@ -10,6 +9,9 @@ import cv2
 import easyocr
 import yaml
 import sqlite3
+import ast
+
+from fuzzywuzzy import fuzz
 config: dict = yaml.load(open('mysticocr.yml', 'r'),
                          Loader=yaml.FullLoader)['mystic']
 
@@ -18,31 +20,33 @@ def main():
 
     if (config.get('command') == "scan"):
         scan()
+    elif (config.get('command') == "match"):
+        match()
 
 
 def scan():
     files = glob.glob(os.path.join(config['scan']['image_dir'], "*.jpg"))
     reader = easyocr.Reader(['en'], gpu=config['scan']['gpu'])
-    count = 0
     db_connection = sqlite3.connect(config['scan']['output_db'])
     db_cursor = db_connection.cursor()
-    done = False
     if config['overwrite_db'] is True:
-        db_cursor.execute('DROP TABLE IF EXISTS ocr_results;')
-    db_cursor.execute(
-        'CREATE TABLE IF NOT EXISTS ocr_results (id INTEGER PRIMARY KEY AUTOINCREMENT, file_name TEXT,card_type TEXT,foil TEXT,set TEXT,rarity TEXT,ocr_result TEXT);')
+        db_connection.execute('DROP TABLE IF EXISTS ocr_results;')
+    db_connection.execute(
+        'CREATE TABLE IF NOT EXISTS ocr_results (id INTEGER PRIMARY KEY AUTOINCREMENT, file_name TEXT,card_type TEXT,foil TEXT,card_set TEXT,rarity TEXT,ocr_result TEXT);')
+    done = False
+    count = 0
 
-    def animated_loading():
+    def animated_loading(length: int):
         for c in itertools.cycle(['|', '/', '-', '\\']):
             if done:
                 break
             sys.stdout.write(
-                f'\rScanning images: {c} {count}/{len(files)} OR {round((count/len(files)*100),3)}% completed')
+                f'\rScanning images: {c} {count}/{length} OR {round((count/length*100),3)}% completed')
             sys.stdout.flush()
             time.sleep(0.1)
-
-    sys.stdout.write('\rDone!            ')
-    t = threading.Thread(target=animated_loading, daemon=True)
+        sys.stdout.write('\rDone!            ')
+    t = threading.Thread(target=animated_loading, args=[
+                         len(files)], daemon=True)
     t.start()
     for file in files:
         count += 1
@@ -65,12 +69,44 @@ def scan():
             cv2.waitKey(1)  # 1 to make sure image updates
 
         db_connection.execute(
-            "INSERT INTO ocr_results (file_name,card_type,foil,set,rarity,ocr_result) VALUES (?,?,?,?,?,?);",
+            "INSERT INTO ocr_results (file_name,card_type,foil,card_set,rarity,ocr_result) VALUES (?,?,?,?,?,?);",
             (file, config['scan']['card']['type'],
              config['scan']['card']['foil'],
              config['scan']['card']['set'],
              config['scan']['card']['rarity'], f'{result}'))
         db_connection.commit()
+    done = True
+
+
+def match():
+    card_db = json.loads(
+        open(config['match']['card_db'], 'r', encoding='utf-8').read())
+    db_connection = sqlite3.connect(config['match']['db'])
+    db_cursor = db_connection.cursor()
+    db_cursor.execute('SELECT ocr_result FROM ocr_results;')
+    ocr_results = db_cursor.fetchall()
+    done = False
+    count = 0
+
+    def animated_loading(length: int):
+        for c in itertools.cycle(['|', '/', '-', '\\']):
+            if done:
+                break
+            sys.stdout.write(
+                f'\rScanning images: {c} {count}/{length} OR {round((count/length*100),3)}% completed')
+            sys.stdout.flush()
+            time.sleep(0.1)
+        sys.stdout.write('\rDone!            ')
+    t = threading.Thread(target=animated_loading, args=[
+                         len(ocr_results)], daemon=True)
+    t.start()
+
+    for result in ocr_results:
+        count += 1
+        highest_ratio = 0
+        ocr_1st_element = ast.literal_eval(result[0])
+        print(ocr_1st_element[0][1])
+    done = True
 
 
 def calc_avg_confidence(result):
