80eebbe675ad049a886d80034684309d29931c08
Chad O <lvnvceo@gmail.com>
2023-06-25 10:49:49 -0700

    Refactored & Modified database

--- MysticOCR.py
+++ MysticOCR.py
@@ -11,6 +11,7 @@ import numpy as np
 import yaml
 import sqlite3
 import ast
+import concurrent.futures
 
 from fuzzywuzzy import fuzz
 config: dict = yaml.load(open('mysticocr.yml', 'r'),
@@ -32,7 +33,7 @@ def scan():
     if config['overwrite_db'] is True:
         db_connection.execute('DROP TABLE IF EXISTS ocr_results;')
     db_connection.execute(
-        'CREATE TABLE IF NOT EXISTS ocr_results (id INTEGER PRIMARY KEY AUTOINCREMENT, file_name TEXT,location TEXT,foil TEXT, showcase TEXT,ocr_result TEXT,pass TEXT,image BLOB);')
+        'CREATE TABLE IF NOT EXISTS ocr_results (id INTEGER PRIMARY KEY AUTOINCREMENT, file_name TEXT,location TEXT,foil TEXT, showcase TEXT,ocr_result TEXT,image BLOB);')
     done = False
     count = 0
 
@@ -73,18 +74,51 @@ def scan():
             cv2.imshow('Image', imagecv)
             cv2.waitKey(1)  # 1 to make sure image updates
         db_connection.execute(
-            "INSERT INTO ocr_results (file_name,location,foil,showcase,ocr_result,pass,image) VALUES (?,?,?,?,?,?,?);",
+            "INSERT INTO ocr_results (file_name,location,foil,showcase,ocr_result,image) VALUES (?,?,?,?,?,?);",
             (file, config['scan']['card']['location'],
-             config['scan']['card']['foil'], config['scan']['card']['showcase'], f'{result}', True, sqlite3.Binary(cv2.imencode(".jpg", originalimagecv)[1].tobytes())))
+             config['scan']['card']['foil'], config['scan']['card']['showcase'], f'{result}', sqlite3.Binary(cv2.imencode(".jpg", originalimagecv)[1].tobytes())))
         db_connection.commit()
     done = True
+
+
+def match_card(card_set, chunk):  # type: ignore
+    count = 0
+    length = len(chunk)
+    highest_cards = []
+    results = []
+    highest_ratios = []
+    for ocr_result in chunk:
+        count += 1
+        highest_ratio = 0
+        highest_card = dict()
+        ocr_1st_element = ast.literal_eval(ocr_result[5])[0][1]
+        ocr_2nd_element = ast.literal_eval(ocr_result[5])[1][1]
+        ocr_3rd_element = ast.literal_eval(ocr_result[5])[2][1]
+        for name in card_set:
+            name = name.split("//")[0]
+            fuzz_ratio_1 = fuzz.ratio(ocr_1st_element, name)
+            fuzz_ratio_2 = fuzz.ratio(ocr_2nd_element, name)
+            fuzz_ratio_3 = fuzz.ratio(
+                " ".join([ocr_1st_element, ocr_2nd_element]), name)
+            fuzz_ratio_4 = fuzz.ratio(
+                " ".join([ocr_1st_element, ocr_2nd_element, ocr_3rd_element]), name)
+            fuzz_ratio = max([fuzz_ratio_1, fuzz_ratio_2,
+                             fuzz_ratio_3, fuzz_ratio_4])
+            if fuzz_ratio > highest_ratio:
+                highest_ratio = fuzz_ratio
+                highest_card = name
+        print(
+            f'highest_ratio: {highest_ratio} :{highest_card} : {count}/{length} : {ocr_result[1]} : CON: {ocr_result[2]}')
+        highest_cards.append(highest_card)
+        results.append(ocr_result)
+        highest_ratios.append(highest_ratio)
+    return [highest_cards, results, highest_ratios]
 
 
 def match():
     card_db: list = json.loads(
         open(config['match']['card_db'], 'r', encoding='utf-8').read())
     card_set = {card.get('name') for card in card_db}
-    print(card_set)
     db_connection = sqlite3.connect(config['match']['db'])
     db_cursor = db_connection.cursor()
     db_cursor.execute(
@@ -94,33 +128,21 @@ def match():
     if (config['overwrite_db'] is True):
         db_connection.execute("DROP TABLE IF EXISTS match_results;")
     db_connection.execute(
-        "CREATE TABLE IF NOT EXISTS match_results (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT,file_name TEXT,foil TEXT, showcase TEXT,location TEXT, ocr_result TEXT);")
-    count = 0
-    length = len(ocr_results)
-    for result in ocr_results:
-        count += 1
-        highest_ratio = 0
-        highest_card = dict()
-        ocr_1st_element = ast.literal_eval(result[5])[0][1]
-        ocr_2nd_element = ast.literal_eval(result[5])[1][1]
-        ocr_3rd_element = ast.literal_eval(result[5])[2][1]
-        for name in card_set:
-            name = name.split("//")[0]
-            fuzz_ratio_1 = fuzz.ratio(ocr_1st_element, name)
-            fuzz_ratio_2 = fuzz.ratio(ocr_2nd_element, name)
-            fuzz_ratio_3 = fuzz.ratio(
-                " ".join([ocr_1st_element, ocr_2nd_element]), name)
-            fuzz_ratio_3 = fuzz.ratio(
-                " ".join([ocr_1st_element, ocr_2nd_element, ocr_3rd_element]), name)
-            fuzz_ratio = max([fuzz_ratio_1, fuzz_ratio_2, fuzz_ratio_3])
-            if fuzz_ratio > highest_ratio:
-                highest_ratio = fuzz_ratio
-                highest_card = name
-        print(
-            f'highest_ratio: {highest_ratio} :{highest_card} : {count}/{length} : {result[1]} : CON: {result[2]}')
-        db_connection.execute("INSERT INTO match_results (name,file_name,location,foil,showcase,ocr_result) VALUES (?,?,?,?,?,?);", (
-                              highest_card, result[1], result[2], result[3], result[4], result[5]))
-        db_connection.commit()
+        "CREATE TABLE IF NOT EXISTS match_results (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT,file_name TEXT,foil TEXT, showcase TEXT,location TEXT, ocr_result TEXT,ratio INT,image BLOB);")
+    chunk_size = 50  # Number of cards to process per chunk
+    ocr_result_chunks = [ocr_results[i:i+chunk_size]
+                         for i in range(0, len(ocr_results), chunk_size)]  # type: ignore
+    with concurrent.futures.ProcessPoolExecutor(max_workers=14) as executor:
+        futures = []
+        for chunk in ocr_result_chunks:
+            futures.append(executor.submit(match_card, card_set, chunk))
+        for future in concurrent.futures.as_completed(futures):
+            highest_cards, ocr_results, highest_ratios = future.result()
+            # type: ignore
+            for highest_card, ocr_result, highest_ratio in zip(highest_cards, ocr_results, highest_ratios):
+                db_connection.execute("INSERT INTO match_results (name,file_name,location,foil,showcase,ocr_result,ratio,image) VALUES (?,?,?,?,?,?,?,?);", (
+                    highest_card, ocr_result[1], ocr_result[2], ocr_result[3], ocr_result[4], ocr_result[5], highest_ratio, ocr_result[6]))
+                db_connection.commit()
 
 
 def calc_avg_confidence(result):
